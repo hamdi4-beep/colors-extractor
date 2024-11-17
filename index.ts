40256cf4b5cf491a5489ce39b6ae664370fd765d
hamdi <hamdi.pirillo4@gmail.com>
2024-11-17 07:33:18 +0100

    improved naming for functions and reduced indentation

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 import { promises as fsPromise } from "fs";
-import { processData } from "./utils/dataProcessing"; 
+import { getColors } from "./utils/dataProcessing"; 
 
 readFile('./assets/style-guide.md', console.log)
 
 async function readFile(path: string, cb: Function) {
     try {
-        const result = await fsPromise.readFile(path, 'utf-8')
-        cb(processData(result))
+        const result = getColors(await fsPromise.readFile(path, 'utf-8'))
+        cb(result)
     } catch (err: any) {
         if (err.code === 'ENOENT') {
             console.log('No such file exists.')
--- utils/dataProcessing.ts
+++ utils/dataProcessing.ts
@@ -3,32 +3,13 @@ const categoryRegex = /^#{3}\s(.+)$/
 
 let category: string
 
-export function processData(data: string) {
+export function getColors(data: string) {
     const lines = data.split('\n').filter(line => stylesRegex.test(line))
-    const values = extractValues()
+    const values = extractValues(lines)
 
     if (!values.length) {
         console.log('None of the targeted categories were found.')
         return
-    }
-
-    function extractValues() {
-        const results = []
-
-        for (const line of lines) {
-            if (categoryRegex.test(line)) category = line.match(categoryRegex)![1].toLowerCase()
-    
-            if (line.startsWith('-')) {
-                const [key, value] = line.split(/:\s?/)
-    
-                if (
-                    category === 'primary' ||
-                    category === 'neutral'
-                ) results.push([category, [convertKey(key.replace('-', '')), value]])
-            }
-        }
-
-        return results.filter(Boolean)
     }
 
     return {
@@ -39,28 +20,43 @@ export function processData(data: string) {
 const convertKey = (key: string) => key.toLowerCase().split(' ').join('-')
 const isObjectEmpty = (obj: Object) => Object.keys(obj).length === 0
 
-const convertArrToObject = (arr: (string | string[])[][]) => {
-    return arr.reduce((prev: Object, curr) => {
-        const [key, [prop, value]] = curr as [string, string[]]
-    
-        if (!isObjectEmpty(prev)) {
-            const colors = (prev as {
-                [x: string]: Object
-            })[key]
+const convertArrToObject = (arr: (string | string[])[][]) => arr.reduce((prev: Object, curr) => {
+    const [key, [prop, value]] = curr as [string, string[]]
 
-            return {
-                ...prev,
-                [key]: {
-                    ...colors,
-                    [prop]: value
-                }
-            }
+    if (isObjectEmpty(prev)) return {
+        [key]: {
+            [prop]: value
         }
-    
-        return {
-            [key]: {
-                [prop]: value
-            }
+    }
+
+    const colors = (prev as {
+        [x: string]: Object
+    })[key]
+
+    return {
+        ...prev,
+        [key]: {
+            ...colors,
+            [prop]: value
         }
-    }, {})
+    }
+}, {})
+
+function extractValues(lines: string[]) {
+    const results = []
+
+    for (const line of lines) {
+        if (categoryRegex.test(line)) category = line.match(categoryRegex)![1].toLowerCase()
+
+        if (line.startsWith('-')) {
+            const [key, value] = line.split(/:\s?/)
+
+            if (
+                category === 'primary' ||
+                category === 'neutral'
+            ) results.push([category, [convertKey(key.replace('-', '')), value]])
+        }
+    }
+
+    return results.filter(Boolean)
 }
